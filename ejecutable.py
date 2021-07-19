c94f1c70c11322fe929cecccde75092cdeb16c8e
profAltamirano <87337266+profAltamirano@users.noreply.github.com>
2021-07-19 00:14:45 -0300

    Add files via upload

---
+++ ejecutable.py
@@ -0,0 +1,8 @@
+from cx_Freeze import setup, Executable
+
+setup(
+    name = "ventana",
+    version = "0.1",
+    description = "ventana",
+    executables = [Executable("detectorBarbijo.py")],
+)
